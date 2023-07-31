7c031f1af3e7f89e07b35d1ab0df24a3678ce320
S4RMI618 <huessabe@gmail.com>
2023-07-31 14:36:14 -0500

    deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+#abort on errors
+set -e
+
+#build
+
+npm run build
+
+# navigate into the build output directory
+
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'ww.example.com' > CNAME
+
+git init 
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:S4RMI618/S4RMI618.github.io.git main
+
+cd -
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "https://S4RMI618.github.io",
+  plugins: [react()]
 })
